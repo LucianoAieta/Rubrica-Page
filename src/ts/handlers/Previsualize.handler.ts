3e8896465cb8e3301ab156ace88882c77c29cfc9
Luciano <luck69aieta06@gmail.com>
2021-08-05 20:33:46 -0300

    Artificial Request added

--- src/ts/handlers/Previsualize.handler.ts
+++ src/ts/handlers/Previsualize.handler.ts
@@ -1,6 +1,23 @@
 import { setLoadingScreen } from '../loading/loading.screen';
 
-export const handlePrevisualizeButton = () => {
-    setLoadingScreen({ opacity: '1', visibility: 'visible' });
-    setTimeout(() => setLoadingScreen({ opacity: '0', visibility: 'hidden' }), 5000);
+const artificialRequest = ({ type }: { type: string }) => {
+    return new Promise<void>((res, rej) => {
+        const Cases = {
+            Correct: () => res(),
+            Error: () => rej('Error!'),
+        };
+
+        setTimeout(() => Cases[type](), 3000);
+    });
+};
+
+export const handlePrevisualizeButton = async () => {
+    try {
+        setLoadingScreen({ opacity: '1', visibility: 'visible' });
+        await artificialRequest({ type: 'Error' });
+        setLoadingScreen({ opacity: '0', visibility: 'hidden' });
+    } catch (error) {
+        console.log(error);
+        setLoadingScreen({ opacity: '0', visibility: 'hidden' });
+    }
 };
